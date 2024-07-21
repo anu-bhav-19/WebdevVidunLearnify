2274c310cce7795c6d69f964f8e83984366559e6
anu-bhav-19 <124033737+anu-bhav-19@users.noreply.github.com>
2024-07-21 23:43:34 +0530

    Add files via upload

---
+++ Assignment-9(DomManipulationUsingJs)/script.js
@@ -0,0 +1,8 @@
+function turnOn(){
+    document.getElementById('image').src='n.jpg';
+    document.body.style.backgroundColor = 'white' ;
+}
+function turnOff(){
+        document.getElementById('image').src='offf.jpg';
+        document.body.style.backgroundColor = 'rgb(34, 29, 29)'
+}
